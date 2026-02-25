eb972689adeee3218cb4a0dd9e2684101a147765
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2026-02-26 00:21:01 +0530

    Made changes in security

--- app/core/security.py
+++ app/core/security.py
@@ -4,17 +4,23 @@ from datetime import datetime, timedelta, timezone
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY='Mein Nhi Bataunga'
-ALGORITHM='HS256'
+SECRET_KEY = "Mein Nhi Bataunga"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 # Function to convert plain password to hash password
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    # 🔒 bcrypt supports max 72 bytes
+    password_bytes = password.encode("utf-8")[:72]
+    return pwd_context.hash(password_bytes)
+
 
 # Function to verify password
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+def verify_password(plain: str, hashed: str):
+    plain_bytes = plain.encode("utf-8")[:72]
+    return pwd_context.verify(plain_bytes, hashed)
+
 
 # Access token
 def create_access_token(data: dict):
